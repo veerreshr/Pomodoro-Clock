3071354627bb09624b563f0ad509afd55e82464a
veeresh <veerreshr@gmail.com>
2020-08-09 20:48:14 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUp,
+  faArrowDown,
+  faPlay,
+  faPause,
+  faSyncAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pomodoro Clock</h1>
+      <div className="length-container">
+        <div className="break">
+          <div id="break-label">Break Length</div>
+          <div className="length">
+            <div id="break-decrement">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </div>
+            <div id="break-length">5</div>
+            <div id="break-increment">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </div>
+          </div>
+        </div>
+        <div className="session">
+          <div id="session-label">Session Length</div>
+          <div className="length">
+            <div id="session-decrement">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </div>
+            <div id="session-length">5</div>
+            <div id="session-increment">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="timer">
+        <div id="timer-label">Session</div>
+        <div id="time-left">25 : 00</div>
+      </div>
+      <div className="options">
+        <div id="start_stop">
+          <FontAwesomeIcon icon={faPlay} />
+          <FontAwesomeIcon icon={faPause} />
+        </div>
+        <div id="reset">
+          <FontAwesomeIcon icon={faSyncAlt} />
+        </div>
+      </div>
     </div>
   );
 }
